08e0623b2e4241679a048f75c26ec657cd3dc111
das361h <das361h@gmail.com>
2025-05-28 14:44:14 +0400

    Function for database wipe

--- app/src/main/java/com/das361h/qr_attendance/teacherPortal.java
+++ app/src/main/java/com/das361h/qr_attendance/teacherPortal.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,25 +17,47 @@ public class teacherPortal extends AppCompatActivity {
     CheckBox checkBox;
     Button wipe;
     EditText retype;
+    SQLiteDBHelper SQLDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_portal);
 
+
         takeAtt=findViewById(R.id.takeAtt);
         takeAtt.setOnClickListener(v -> {
             startActivity(new Intent(teacherPortal.this, TakeAttendance.class));
         });
+
 
         viewWeekly=findViewById(R.id.viewWeekly);
         viewWeekly.setOnClickListener(v -> {
             startActivity(new Intent(teacherPortal.this, viewWeekly.class));
         });
 
+
         viewStudent=findViewById(R.id.viewStudent);
         viewStudent.setOnClickListener(v -> {
             startActivity(new Intent(teacherPortal.this, ViewStudent.class));
         });
+
+
+        wipe=findViewById(R.id.wipe);
+        checkBox=findViewById(R.id.checkBox);
+        retype=findViewById(R.id.retype);
+        SQLDB = new SQLiteDBHelper(this);
+        wipe.setOnClickListener(v -> {
+            if(checkBox.isChecked() && retype.getText().toString().equals("I am sure")){
+                SQLDB.deleteAllRecord();
+                checkBox.setChecked(false);
+                retype.setText("");
+                Toast.makeText(teacherPortal.this, "Database wiped", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(teacherPortal.this, "Read the instructions carefully", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
